c490886a1fe02e566c60c66d3632545355fe8e0d
Deolu_Josef <kiishijoseph@gmail.com>
2022-07-01 00:31:43 +0100

    Completed the Home page, About us and Road Map page, added animating effect to the vector

---
+++ src/components/sections/Roadmap.jsx
@@ -0,0 +1,211 @@
+import React, { useRef, useLayoutEffect } from 'react';
+
+import styled from 'styled-components';
+
+import DrawSvg from '../DrawSvg';
+
+import gsap from 'gsap';
+
+import { ScrollTrigger } from 'gsap/all';
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.text};
+  width: fit-content;
+`;
+
+const Container = styled.div`
+  width: 70%;
+  height: 200vh;
+  background-color: ${(props) => props.theme.body};
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SvgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Items = styled.ul`
+  list-style: none;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* background-color: lightblue; */
+
+  & > *:nth-of-type(2n + 1) {
+    justify-content: start;
+
+    div {
+      border-radius: 50px 0 50px 0;
+      text-align: right;
+    }
+    p {
+      border-radius: 40px 0 40px 0;
+    }
+  }
+
+  & > *:nth-of-type(2n) {
+    justify-content: end;
+
+    div {
+      border-radius: 0 50px 0 50px;
+      text-align: left;
+    }
+
+    p {
+      border-radius: 0 40px 0 40px;
+    }
+  }
+`;
+
+const Item = styled.li`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+const ItemContainer = styled.div`
+  width: 40%;
+  height: fit-content;
+  padding: 1rem;
+  border: 3px solid ${(props) => props.theme.text};
+`;
+
+const Box = styled.p`
+  height: fit-content;
+  background-color: ${(props) => props.theme.carouselColor};
+  color: ${(props) => props.theme.text};
+  padding: 1rem;
+  position: relative;
+  border: 1px solid ${(props) => props.theme.text};
+`;
+const SubTitle = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme?.text};
+`;
+
+const Text = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontsm};
+  text-transform: capitalize;
+  color: ${(props) => props.theme?.text};
+
+  font-weight: 400;
+  margin: 0.5rem 0;
+`;
+
+const RoadMapItem = ({ title, subtext, addToRefs }) => {
+  return (
+    <Item ref={addToRefs}>
+      <ItemContainer>
+        <Box>
+          <SubTitle>{title}</SubTitle>
+          <Text>{subtext}</Text>
+        </Box>
+      </ItemContainer>
+    </Item>
+  );
+};
+
+const Roadmap = () => {
+  const reavealRefs = useRef([]);
+  reavealRefs.current = [];
+  gsap.registerPlugin(ScrollTrigger);
+
+  const addToRefs = (el) => {
+    if (el && !reavealRefs.current.includes(el)) {
+      reavealRefs.current.push(el);
+    }
+  };
+
+  useLayoutEffect(() => {
+    let t1 = gsap.timeline();
+    reavealRefs.current.forEach((el, index) => {
+      t1.fromTo(
+        el.childNodes[0],
+        {
+          y: '0',
+        },
+        {
+          y: '-30%',
+
+          scrollTrigger: {
+            id: `section-${index + 1}`,
+            trigger: el,
+            start: 'top center+=200px',
+            end:'bottom center',
+            scrub:true,
+            markers:true,
+          },
+        }
+      );
+    });
+
+    return () => {};
+  }, []);
+
+  return (
+    <Section>
+      <Title>Roadmap</Title>
+      <Container>
+        <SvgContainer>
+          <DrawSvg />
+        </SvgContainer>
+        <Items>
+          <Item>&nbsp;</Item>
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Grand Opening"
+            subtext="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Great Benefits"
+            subtext=" Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Pla"
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Early Access"
+            subtext=" Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Pla"
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="New Merch"
+            subtext=" Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Pla"
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Holders Ranking"
+            subtext=" Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Pla"
+          />
+        </Items>
+      </Container>
+    </Section>
+  );
+};
+
+export default Roadmap;
